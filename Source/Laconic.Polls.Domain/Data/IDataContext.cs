165cafb8e68ef36416b09cedbb75fce6aa6234de
Alexei Matrosov <alexei@matrosov.by>
2016-05-20 12:31:42 +0300

    Added data abstractions

---
+++ Source/Laconic.Polls.Domain/Data/IDataContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Laconic.Polls.Domain.Entities;
+
+namespace Laconic.Polls.Domain.Data
+{
+    public interface IDataContext
+    {
+        IDataSet<Poll> Polls { get; }
+        IDataSet<Option> Options { get; }
+        IDataSet<Vote> Votes { get; }
+        Task SaveChangesAsync();
+    }
+}
---
+++ Source/Laconic.Polls.Domain/Data/IDataSet.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Laconic.Polls.Domain.Entities;
+
+namespace Laconic.Polls.Domain.Data
+{
+    public interface IDataSet<TEntity>
+        where TEntity : Entity
+    {
+        Task<TEntity> FindAsync(int id);
+        void Add(TEntity entity);
+    }
+}
